936d9b9b35a01e5cb5736defcf940bf9714404fd
Sergio Diosdado <sgdc_98@hotmail.com>
2020-09-01 20:05:55 -0500

    Clima function now returns symbol instead of printing it

--- main.rkt
+++ main.rkt
@@ -2,12 +2,12 @@
 
 ; 1. Función clima: Recibe un número que representa la temperatura en centígrados y regresa un símbolo
 (define (clima grados)
-  (cond [(<= grados 0 ) printf 'Congelado]
-        [(<= grados 10) printf 'Helado]
-        [(<= grados 20) printf 'Frío]
-        [(<= grados 30) printf 'Normal]
-        [(<= grados 40) printf 'Caliente]
-        [(> grados 40) printf 'Hirviendo]
+  (cond [(<= grados 0 ) 'Congelado]
+        [(<= grados 10) 'Helado]
+        [(<= grados 20) 'Frío]
+        [(<= grados 30) 'Normal]
+        [(<= grados 40) 'Caliente]
+        [(> grados 40) 'Hirviendo]
    ))
 
 ; 4. Función sumarpar: recibe 4 argumentos numéricos y regrese la suma de los argumentos pares
