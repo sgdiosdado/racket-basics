966ddaad12f1821e3b560702c6c91a80a5355d25
Iñaki Janeiro Olague <inaki_janeiro@live.com.mx>
2020-09-01 20:06:45 -0500

    Ejercicio 2 y 3

--- main.rkt
+++ main.rkt
@@ -10,6 +10,22 @@
         [(> grados 40) printf 'Hirviendo]
    ))
 
+; 2. Función cuadrante: Recibe dos argumentos numéricos representando 'x' y 'y' y regresa el cuadrante del plano cartesiando en el que se encuentra
+(define (cuadrante x y)
+  (cond [(and(zero? x) (zero? y) 'origen)]
+        [(and(positive? x)(positive? y)  'primer_cuadrante)]
+        [(and(negative? x)(positive? y) 'segundo_cuadrante)]
+        [(and(negative? x)(negative? y)  'tercer_cuadrante)]
+        [(and(positive? x)(negative? y)  'cuarto_cuadrante)]
+  )
+)
+
+; 3. Función ordena: Recibe 3 argumentos numéricos y los despliega ordenados de menor a mayor
+
+(define (ordena a b c)
+  (printf "~s\n" (sort (list a b c) <))
+)
+
 ; 4. Función sumarpar: recibe 4 argumentos numéricos y regrese la suma de los argumentos pares
 (define (sumarpar a b c d)
   (apply + (filter even? (list a b c d)))
